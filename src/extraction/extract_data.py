66669eb20324bbfb624bdb84977f36af6ba50201
Reinaldo Rivero <reinaldo.rivero.ro@gmail.com>
2024-09-01 18:30:25 -0400

    Adding examples to extract_data.py and transform_data.py files

---
+++ src/extraction/extract_data.py
@@ -0,0 +1,11 @@
+import logging
+
+def extract_from_source_1():
+    logging.info("Extrayendo datos de la fuente 1")
+    # Código para extraer datos de la fuente 1
+    return data_source_1
+
+def extract_from_source_2():
+    logging.info("Extrayendo datos de la fuente 2")
+    # Código para extraer datos de la fuente 2
+    return data_source_2
---
+++ src/transformation/transform_data.py
@@ -0,0 +1,6 @@
+import logging
+
+def transform_data(data):
+    logging.info("Transformando datos")
+    # Código para transformar los datos
+    return transformed_data
